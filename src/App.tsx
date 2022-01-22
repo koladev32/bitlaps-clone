acf08df85ef8f309948417138673ec583316e7bb
koladev <onaelmangabo@gmail.com>
2022-01-22 17:44:11 +0100

    - feat: add tabicons to nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
+import {
+    faShoppingBasket,
+    faThLarge,
+    faWallet,
+    faExchangeAlt,
+    faChartLine,
+    IconDefinition
+} from '@fortawesome/free-solid-svg-icons'
 import React from 'react';
+import TabIconNav from "./components/TabIconNav";
+
+const tabIcons:Array<IconDefinition> = [faShoppingBasket, faThLarge, faWallet, faExchangeAlt, faChartLine]
 
 export default function App() {
     return (
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
+        <div>
+            <nav className="columns-3 item mx-8 h-10 my-2">
+                <div className="flex justify-start space-x-16">
+                    {
+                        tabIcons.map((icon, index) => (
+                            <TabIconNav icon={icon} key={index}/>
+                        ))
+                    }
+                </div>
+                <div className="text-center">
+                    Bitlaps
+                </div>
+                <div className="flex justify-end">
+                    Nav links
+                </div>
+            </nav>
+        </div>
     )
 }
