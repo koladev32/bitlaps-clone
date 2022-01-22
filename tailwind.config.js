335e0c561882aa2719bd322f62ee2baa9b3e94a8
koladev <onaelmangabo@gmail.com>
2022-01-22 12:41:03 +0100

    - feat: add tailwind configs for ts and tsx files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,1 @@
-module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+module.exports = {content: ["./src/**/*.{js,jsx,ts,tsx}",], theme: {extend: {},}, plugins: [],}
